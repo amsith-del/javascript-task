050985ae0eef578c8de44f15cda6784031600acd
amsith-del <amsith@paithacs.com>
2026-05-26 19:30:10 +0530

    Javascript basic task

--- script.js
+++ script.js
@@ -12,6 +12,8 @@
 // }
 
 
+
+
 // Password check using while
 
 // let password = "";
@@ -32,17 +34,89 @@
 // console.log("Sum =", num1 + num2);
 
 
+
+
 // Age Validator
 
 
-let age = Number(prompt("Enter your age"));
+// let age = Number(prompt("Enter your age"));
 
-if (age < 0) {
-    console.log("Invalid age");
-}
-else if (age >= 18) {
-    console.log("Eligible to Vote");
-}
-else {
-    console.log("Not eligible to vote");
-}
+// if (age < 0) {
+//     console.log("Invalid age");
+// }
+// else if (age >= 18) {
+//     console.log("Eligible to Vote");
+// }
+// else {
+//     console.log("Not eligible to vote");
+// }
+
+
+
+
+// Even or Odd Checker
+
+
+// let number = Number(prompt("Enter a number"));
+
+// if (number % 2 === 0) {
+//     console.log("Even");
+// }
+// else {
+//     console.log("Odd");
+// }
+
+// let a=Number(prompt("Enter first number"))
+// let b=Number(prompt("Enter second number"))
+
+
+// Largest of Two Numbers
+
+// if (a>b){
+//     console.log(a+"is greater")
+// }
+// else if (b>a){
+//     console.log(b+"is greater")
+// }
+// else {
+//     console.log("Both are equal number")
+// }
+
+// Default Username (Nullish Colesching)
+
+// let username = null;
+// let displayName = username ?? "Guest";
+// console.log(displayName);
+
+
+// Print Numbers from 1 to 10
+// using for loop
+
+// for (let i = 1; i <= 10; i=i+1) {
+//     console.log(i);
+// }
+
+// Print Even Numbers using for loop
+
+// for (let i = 2; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+
+// // Print Even Numbers using while loop
+
+// let i = 2;
+
+// while (i <= 20) {
+//     console.log(i);
+//     i += 2;
+// }
+
+// Countdown Using while
+
+// let num = 10;
+
+// while (num >= 1) {
+//     console.log(num);
+//     num--;
+// }
